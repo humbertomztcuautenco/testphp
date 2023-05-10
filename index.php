3b2caee7fe2fc23bc8bb84d8e7f39f41ba1f29c2
Humberto <humbertmtzcuautenco@hotmail.com>
2023-05-09 23:55:11 -0600

    add list data
    commit reg cambios

--- index.php
+++ index.php
@@ -12,8 +12,8 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 //env 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+// $dotenv->load();
 
 //* crear app y activar reescritura
 
@@ -30,10 +30,37 @@ if (PHP_SAPI == 'cli-server') {
 require __DIR__ . '/src/loadApp.php';
 
 $app->get('/', function (Request $request, Response $response) {
-    $dbHost = $_ENV['HOST'];
+    // $dbHost = $_ENV['HOST'];
     $response->withHeader('Content-type','application/json')
              ->getBody()->write("Slim 4 ");
     return $response;
+});
+
+$app->get('/list', function (Request $request, Response $response) {
+    // Lee el contenido del archivo HTML
+    $content = file_get_contents('archivo.html');
+
+    // Devuelve el contenido como respuesta
+    $response->getBody()->write($content);
+
+    return $response;
+});
+
+$app->post('/', function (Request $req, Response $res, $args) {
+    $parametros = json_decode($req->getBody()->getContents());
+    $text = $parametros->text;
+    $date = $parametros->date;
+    // Abre el archivo en modo escritura
+    $archivo = fopen('archivo.html', 'a');
+
+    // Escribe el texto en el archivo
+    fwrite($archivo, "$text - $date \n");
+
+    // Cierra el archivo
+    fclose($archivo);
+    $res->withHeader('Content-type','application/json')
+             ->getBody()->write("Slim 4 ");
+    return $res;
 });
 
 $app->run();
